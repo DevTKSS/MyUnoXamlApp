4301fa25027dada619697c0a9acf0a1f24341da3
DevTKSS <info@technische-konstruktion.com>
2024-11-05 23:05:52 +0100

    Changed MainModel Type in MainPage to MainViewModel, now the application do not longer build because of not known "InitilizeComponent()" and in xaml defined x:Names not longer known issue

--- MyUnoXamlApp/Presentation/MainPage.xaml.cs
+++ MyUnoXamlApp/Presentation/MainPage.xaml.cs
@@ -11,7 +11,7 @@ namespace MyUnoXamlApp.Presentation;
 [Bindable]
 public sealed partial class MainPage : Page
 {
-    public MainModel Model => (MainModel)DataContext;
+    public MainViewModel Model => (MainViewModel)DataContext;
     private readonly HashSet<uint> activePointerIds = new HashSet<uint>();
     private bool isPointerCaptured = false;
 
